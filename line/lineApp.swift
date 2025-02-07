e765c8424b9790da341e4281093f6905e170f55c
Hideto Higashi <hideto1198@yahoo.co.jp>
2025-02-08 00:12:04 +0900

    Initial commit

---
+++ line/lineApp.swift
@@ -0,0 +1,17 @@
+//
+//  lineApp.swift
+//  line
+//
+//  Created by 東　秀斗 on 2025/02/08.
+//
+
+import SwiftUI
+
+@main
+struct lineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lineTests/lineTests.swift
@@ -0,0 +1,16 @@
+//
+//  lineTests.swift
+//  lineTests
+//
+//  Created by 東　秀斗 on 2025/02/08.
+//
+
+import Testing
+
+struct lineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
