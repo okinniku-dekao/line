2d219f6283c8c2682a4f9c7a797252482e95c10d
hideto1198 <hideto1198@yahoo.co.jp>
2025-03-02 02:16:45 +0900

    HomeTop追加

---
+++ iOS/line/Sources/Presentation/HomeFeature/Sources/HomeTop/Feature/HomeTopAction.swift
@@ -0,0 +1,12 @@
+//
+//  HomeTopAction.swift
+//  Home
+//
+//  Created by 東　秀斗 on 2025/03/02.
+//
+
+import ComposableArchitecture
+
+extension HomeTopFeature {
+    public enum Action: Equatable {}
+}
