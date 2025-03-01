9976b3ccdc140e23adb12566a2963a2ad6a9f337
hideto1198 <hideto1198@yahoo.co.jp>
2025-03-02 00:06:49 +0900

    featuresフォルダを追加

---
+++ iOS/line/Sources/Features/News/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NewsFeature",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "NewsFeature",
+            targets: ["NewsFeature"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            from: "1.17.1"
+        )
+    ],
+    targets: [
+        .target(
+            name: "NewsFeature",
+            dependencies: [
+                .product(
+                    name: "ComposableArchitecture",
+                    package: "swift-composable-architecture"
+                ),
+            ]
+        ),
+        .testTarget(
+            name: "NewsFeatureTests",
+            dependencies: ["NewsFeature"]
+        ),
+    ]
+)
---
+++ iOS/line/Sources/Features/Wallet/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "WalletFeature",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(name: "WalletFeature", targets: ["WalletFeature"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            from: "1.17.1"
+        )
+    ],
+    targets: [
+        .target(
+            name: "WalletFeature",
+            dependencies: [
+                .product(
+                    name: "ComposableArchitecture",
+                    package: "swift-composable-architecture"
+                ),
+            ]
+        ),
+        .testTarget(
+            name: "WalletFeatureTests",
+            dependencies: ["WalletFeature"]
+        ),
+    ]
+)
