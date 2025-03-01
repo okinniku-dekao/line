46f99c9d1d05e1de446928048f1073416e5c7e69
hideto1198 <hideto1198@yahoo.co.jp>
2025-03-01 21:17:43 +0900

    メインタブを追加

---
+++ iOS/line/Sources/Presentation/Sources/AppRoot/Feature/AppRootFeature.swift
@@ -0,0 +1,7 @@
+//
+//  AppRootFeature.swift
+//  Presentation
+//
+//  Created by 東　秀斗 on 2025/03/01.
+//
+
---
+++ iOS/line/Sources/Presentation/Sources/MainTabBar/Feature/MainTabBarAction.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabBarAction.swift
+//  Presentation
+//
+//  Created by 東　秀斗 on 2025/03/01.
+//
+
+import ComposableArchitecture
+
+extension MainTabBarFeature {
+    enum Action: Equatable, BindableAction {
+        case binding(BindingAction<State>)
+    }
+}
