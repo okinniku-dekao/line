74ac169306e82bd05f7c1cb8dc8d411683b8e986
hideto1198 <hideto1198@yahoo.co.jp>
2025-03-08 01:56:06 +0900

    Equatableにじゅんひょ

--- iOS/line/Sources/Infrastructure/Sources/Error/NetworkError.swift
+++ iOS/line/Sources/Infrastructure/Sources/Error/NetworkError.swift
@@ -7,12 +7,36 @@
 
 import Foundation
 
-enum NetworkError: Error {
+enum NetworkError: Error, Equatable {
     case invalidURL
+    case invalidBaseURL
+    case invalidAPIPath
     case invalidResponse
     case requestFailed(Error)
     case decodingFailed(Error)
     case serverError(statusCode: Int, data: Data?)
     case noData
     case unauthorized
+    case cancelled
+    
+    static func == (lhs: NetworkError, rhs: NetworkError) -> Bool {
+        switch (lhs, rhs) {
+        case (.invalidURL, .invalidURL),
+            (.invalidBaseURL, .invalidBaseURL),
+            (.invalidAPIPath, .invalidAPIPath),
+            (.invalidResponse, .invalidResponse),
+            (.requestFailed, .requestFailed),
+            (.noData, .noData),
+            (.unauthorized, .unauthorized),
+            (.cancelled, .cancelled):
+            return true
+        
+        case let (.serverError(lStatusCode, lData), .serverError(rStatusCode, rData)):
+            return lStatusCode == rStatusCode && lData == rData
+            
+        default:
+            return false
+            
+        }
+    }
 }
