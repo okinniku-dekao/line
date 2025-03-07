9b6240fd5290e709ab4f772dba834aeb9dbbbcd4
hideto1198 <hideto1198@yahoo.co.jp>
2025-03-08 01:57:50 +0900

    iOS17以上に変更

--- iOS/line/Sources/Infrastructure/Package.swift
+++ iOS/line/Sources/Infrastructure/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Infrastructure",
-    platforms: [.iOS(.v14)],    
+    platforms: [.iOS(.v17)],
     products: [
         .library(name: "Infrastructure", targets: ["Infrastructure"])
     ],
