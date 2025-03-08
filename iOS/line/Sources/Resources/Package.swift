5b664956b4eee2785ab555b8cc598a60faaa9350
hideto1198 <hideto1198@yahoo.co.jp>
2025-03-09 01:38:15 +0900

    Resourcesパッケージ追加

---
+++ iOS/line/Sources/Resources/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Resources",
+    platforms: [.iOS(.v17)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Resources",
+            targets: ["Resources"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.8.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Resources",
+            dependencies: [.product(name: "RswiftLibrary", package: "R.swift")],
+            resources: [.process("Resources")],
+            plugins: [.plugin(name: "RswiftGeneratePublicResources", package: "R.swift")]
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ iOS/line/Sources/Resources/Sources/Resources/Resources.swift
@@ -0,0 +1,8 @@
+//
+//  Resources.swift
+//  Resources
+//
+//  Created by 東　秀斗 on 2025/03/04.
+//
+
+@_exported import RswiftResources
