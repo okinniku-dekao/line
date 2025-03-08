9e0954392d80e1443645d6d584bef5aa8c363d54
hideto1198 <hideto1198@yahoo.co.jp>
2025-03-08 13:56:22 +0900

    Entity追加

---
+++ iOS/line/Sources/Domain/Sources/Domain/Entities/User/AuthResponse.swift
@@ -0,0 +1,12 @@
+//
+//  AuthResponse.swift
+//  Domain
+//
+//  Created by 東　秀斗 on 2025/03/08.
+//
+
+
+public struct AuthResponse: Equatable {
+    let user: User
+    let message: String
+}
---
+++ iOS/line/Sources/Domain/Sources/Domain/Entities/User/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Domain
+//
+//  Created by 東　秀斗 on 2025/03/08.
+//
+
+import Foundation
+
+public struct User: Equatable {
+    let id: UUID
+    let email: String
+}
